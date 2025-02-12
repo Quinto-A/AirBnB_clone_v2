568e7b0b38c134e687aca93d2b133e56976ef6f5
Quinto-A <acquinoquinto5@gmail.com>
2025-02-12 23:32:28 +0300

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,8 @@ def display2(text):
     """creates static routing"""
     return f'C {text}'
 
-@app.route("/python/<text>", defaults={'text' : 'is cool'}, strict_slashes=False)
+@app.route("/python/", defaults={'text' : 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def display3(text):
     """creates static routing"""
     text = text.replace("_", " ")
