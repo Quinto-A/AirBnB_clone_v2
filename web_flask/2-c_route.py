9c247b564d1728edd39898205f9e27403b8f591e
Quinto-A <acquinoquinto5@gmail.com>
2025-02-13 12:21:23 +0300

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,21 +5,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def display():
     """Creates dynamic routing"""
     return "Hello HBNB!"
+
 
 @app.route("/HBNB", strict_slashes=False)
 def display1():
     """creates static routing"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def display2(text):
     """Creates static routing"""
     text = text.replace("_", " ")
     return f'C {text}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
