ee735401fecc82cd18b962c3f15f6e1f380c2a34
Quinto-A <acquinoquinto5@gmail.com>
2025-02-13 12:34:15 +0300

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,15 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def display():
     """Creates dynamic routing"""
     return "Hello HBNB!"
 
+
 @app.route("/HBNB", strict_slashes=False)
 def display1():
     """creates static routing"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def display2(text):
@@ -18,14 +21,19 @@ def display2(text):
     """creates static routing"""
     return f'C {text}'
 
-@app.route("/python/", defaults={'text' : 'is cool'}, strict_slashes=False)
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display3(text):
     """creates static routing"""
     text = text.replace("_", " ")
     return f'Python {text}'
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def display4(n):
     """displays only if n is an integer"""
     return f'{n} is a number'
+
+if __name__ == "__main__":
+    app.run(debug=True)
