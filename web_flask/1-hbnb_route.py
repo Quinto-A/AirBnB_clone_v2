e35126bd68ec44407d1480b25b911d5ed759abaa
Quinto-A <acquinoquinto5@gmail.com>
2025-02-13 12:19:09 +0300

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,15 +5,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def display():
     """Creates dynamic routing"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def display1():
     """creates static routing"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
